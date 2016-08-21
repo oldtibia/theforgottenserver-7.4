5db59a2f9dad4119bdd701e2a5090b96f0c1a65a
peonso <peonso@hotmail.com>
2016-08-21 19:48:27 -0400

    Spells fixes
    physical spells blocked by armor
    exori mana cost fixed
    burst arrow oldschool formula
    area of spell fixed

--- server/data/spells/scripts/attack/sudden death.lua
+++ server/data/spells/scripts/attack/sudden death.lua
@@ -1,5 +1,6 @@
 local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_PHYSICALDAMAGE)
+setCombatParam(combat, COMBAT_PARAM_BLOCKARMOR, true)
 setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_MORTAREA)
 setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_DEATH)
 
