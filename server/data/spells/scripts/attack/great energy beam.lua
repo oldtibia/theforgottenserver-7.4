d6d435b1a6d5529dddd4f6955225ad4ee0e7c258
Seliro <emil92b2@gmail.com>
2016-08-22 05:03:32 +0200

    great energy beam

--- server/data/spells/scripts/attack/great energy beam.lua
+++ server/data/spells/scripts/attack/great energy beam.lua
@@ -1,6 +1,6 @@
 local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_ENERGYDAMAGE)
-setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_ENERGYHIT)
+setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_EXPLOSIONHIT)
 
 local area = createCombatArea(AREA_BEAM7, AREADIAGONAL_BEAM7)
 setCombatArea(combat, area)
