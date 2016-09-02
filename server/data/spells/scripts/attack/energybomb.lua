7c792b7262bb902af37cc8598f8780bba9ca3528
Seliro <emil92b2@gmail.com>
2016-09-02 02:54:10 +0200

    updated attack spells

--- server/data/spells/scripts/attack/energybomb.lua
+++ server/data/spells/scripts/attack/energybomb.lua
@@ -5,15 +5,15 @@ combat:setParameter(COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_ENERGY)
 combat:setParameter(COMBAT_PARAM_CREATEITEM, ITEM_ENERGYFIELD_PVP)
 
 local area = createCombatArea(AREA_SQUARE1X1)
-setCombatArea(combat, area)
+combat:setArea(area)
 
-function onCastSpell(cid, var, isHotkey)
+function onCastSpell(creature, var)
 	-- check for stairHop delay
-	if not getCreatureCondition(cid, CONDITION_PACIFIED) then
-		return doCombat(cid, combat, var)
+	if not getCreatureCondition(creature, CONDITION_PACIFIED) then
+		return combat:execute(creature, var)
 	else
-		cid:sendCancelMessage(RETURNVALUE_YOUAREEXHAUSTED)
-		cid:getPosition():sendMagicEffect(CONST_ME_POFF)
+		creature:sendCancelMessage(RETURNVALUE_YOUAREEXHAUSTED)
+		creature:getPosition():sendMagicEffect(CONST_ME_POFF)
 		return false
 	end
 end
